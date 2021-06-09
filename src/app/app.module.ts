123050eef723c549e5a85df69a6a9da83efc821b
marjangoodarzi@gmail.com <marjangoodarzi@gmail.com>
2021-06-09 14:30:01 +0430

    20210906

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './session12/home/home/home.component';
+import { IncComponent } from './session12/inc/inc/inc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IncComponent
   ],
   imports: [
     BrowserModule,
